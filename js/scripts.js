1954807d8f5b22015d144d722d75ee3d0182fbc3
Felipe Vieira <felipvieiraa@gmail.com>
2023-07-01 01:26:49 -0300

    finished

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function listarDados(data) {
+  const dataList = document.getElementById("product-list");
+  dataList.innerHTML = "";
+
+  data.forEach((item) => {
+    const h3 = document.createElement("h3");
+    h3.textContent = item.name;
+    const img = document.createElement("img");
+    img.src = item.image;
+
+    const link = document.createElement("a");
+    link.classList.add("product-item");
+    link.appendChild(img);
+    link.appendChild(h3);
+    link.href = item.link;
+
+    dataList.appendChild(link);
+  });
+}
+
+function fazerRequisicao() {
+  const url = "http://localhost:3000/products"; // Substitua pela URL da sua API
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Chamar a função para listar os dados no HTML
+      listarDados(data);
+    })
+    .catch((error) => {
+      console.error("Erro na requisição:", error);
+    });
+}
+
+fazerRequisicao();
